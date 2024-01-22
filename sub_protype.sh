a098bba615515b2007be8fede1502f0c8b061388
Simone Ciarella <sciarella@login03.delftblue.tudelft.nl>
2024-01-22 13:59:20 +0100

    add sbatch example

---
+++ sub_protype.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+#SBATCH --job-name=MALES-prototype
+#SBATCH --output=MALES-prototype.out
+#SBATCH --error=MALES-prototype.err
+#SBATCH --partition=gpu
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=1G
+#SBATCH --gpus-per-task=1
+#SBATCH --account=research-AE-SPE
+
+source /scratch/sciarella/.virtualenvs/males-blue/bin/activate
+
+python Prototype.py 

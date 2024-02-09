790883797f1499584ab84df2896451841fbdd4a3
SCiarella <simoneciarella@gmail.com>
2024-02-09 15:38:11 +0100

    Update README with yaml info
    Add script to create notebooks

--- Prototype.py
+++ Prototype.py
@@ -1,79 +1,66 @@
-#!/usr/bin/env python
-# coding: utf-8
+## Prototype of the Machine Learning Pipeline
 
-# In[1]:
 
 # Load the necessary packages
 import sys
 import torch
 from speckcn2 import *
 
-# In[2]:
 
 # Load the configuration file
+# this file contains all the information related to how to preprocess the data and which model to use
 config = load_config(sys.argv[1])
-#config = load_config('configuration.yaml')
-
-# In[3]:
-
-# Get some parameters from YAML file
+# one of which is the path to the data, that we need to store in this variable
 datadirectory = config['speckle']['datadirectory']
 
-# In[4]:
 
-# Set the device
+# Set the device: if a GPU is available, we use it
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Using {device}.', flush=True)
 
-# In[5]:
 
-# Load or preprocess the data
+# Preprocess the data
+# if a database of the preprocess data exist, then we just load it 
 all_images, all_tags = prepare_data(config, nimg_print=15)
 
-# In[7]:
 
 # Normalize the tags between 0 and 1
-#dataset, normalize_tag, recover_tag = normalize_tags(all_images, all_tags)
+# this will help the model by giving it more reasonable numbers to work with
 dataset, normalize_img, recover_img, normalize_tag, recover_tag = normalize_imgs_and_tags(
     all_images, all_tags)
 
-# In[8]:
 
 # Split the data in training and testing
 train_loader, test_loader = train_test_split(dataset,
                                              config['hyppar']['batch_size'],
                                              0.8)
 
-# In[9]:
-
 # Load the model that you want to use
 model, last_model_state = setup_model(config)
 # and set the model to run on the device
 model = model.to(device)
 
-# In[10]:
 
 # Define the loss function and optimizer
 criterion = setup_loss(config)
 optimizer = setup_optimizer(config, model)
 
-# In[11]:
 
-# Train the model...
+# (!) Train the model
 model, average_loss = train(model, last_model_state, config, train_loader,
                             test_loader, device, optimizer, criterion)
 print(f'Finished Training, Loss: {average_loss:.5f}', flush=True)
 
-# In[12]:
 
-# Now test the model
+# Now test the model, while also producing some plots
 test_tags = score(model, test_loader, device, criterion, recover_tag,
                   datadirectory)
 
-# In[13]:
 
-# Print some statistics of the screen tags
+# Finaly we do some postprocessing analysis
+# Plot the distribution of the screen tags
 tags_distribution(config, dataset, test_tags, device)
-
+# Plot the loss during training
 plot_loss(config, model, datadirectory)
+# Plot the execution time
 plot_time(config, model, datadirectory)
---
+++ py_to_ipynb.py
@@ -0,0 +1,17 @@
+import os
+
+def convert_py_to_ipynb(py_file, ipynb_file):
+    os.system(f'p2j {py_file} -t {ipynb_file}')
+
+def convert_all_py_to_ipynb(directory):
+    current_script = os.path.basename(__file__)
+    for filename in os.listdir(directory):
+        if filename.endswith('.py') and filename != current_script:
+            py_file = os.path.join(directory, filename)
+            ipynb_file = 'notebooks/'+ os.path.splitext(py_file)[0] + '.ipynb'
+            convert_py_to_ipynb(py_file, ipynb_file)
+            print(f"Converted {py_file} to {ipynb_file}")
+
+directory = '.'
+convert_all_py_to_ipynb(directory)
+
